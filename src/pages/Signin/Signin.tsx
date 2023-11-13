c8186047c73bf575b2352fc47e4d2e822305fcf3
tainv2002 <taivannho5a@gmail.com>
2023-11-13 21:58:05 +0700

    Code UI Sign up page

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,14 +1,13 @@
-import { yupResolver } from '@hookform/resolvers/yup'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { IconButton, Input, Typography, Button } from '@material-tailwind/react'
 import { useForm } from 'react-hook-form'
 import { FaFacebookF } from 'react-icons/fa6'
 import { FcGoogle } from 'react-icons/fc'
 import { Link } from 'react-router-dom'
 import path from 'src/constants/path'
-import { AuthSchema, authSchema } from 'src/utils/rules'
+import { LoginSchema, loginSchema } from 'src/utils/rules'
 
-type FormData = Pick<AuthSchema, 'email' | 'password'>
-const loginSchema = authSchema.pick(['email', 'password'])
+type FormData = LoginSchema
 
 function Signin() {
   const {
@@ -21,45 +20,47 @@ function Signin() {
       email: '',
       password: ''
     },
-    resolver: yupResolver(loginSchema)
+    resolver: zodResolver(loginSchema)
   })
 
   const onSubmit = handleSubmit((data) => {
     console.log(data)
   })
 
+  console.log(errors)
+
   return (
     <>
       <div className='flex items-center'>
         <p className='mb-0 mr-4 text-lg font-normal'>Sign in with</p>
         <div className='flex items-center gap-2'>
-          <IconButton className='bg-primary h-8 w-8 rounded-full text-white'>
+          <IconButton className='h-8 w-8 rounded-full bg-primary text-white'>
             <FaFacebookF />
           </IconButton>
 
-          <IconButton className='bg-primary h-8 w-8 rounded-full text-white'>
+          <IconButton className='h-8 w-8 rounded-full bg-primary text-white'>
             <FcGoogle />
           </IconButton>
         </div>
       </div>
 
       <div className='my-4 flex items-center gap-3'>
-        <div className='bg-blue-gray-200 h-[1px] flex-1'></div>
+        <div className='h-[1px] flex-1 bg-blue-gray-200'></div>
 
         <p className='text-sm font-semibold'>Or</p>
 
-        <div className='bg-blue-gray-200 h-[1px] flex-1'></div>
+        <div className='h-[1px] flex-1 bg-blue-gray-200'></div>
       </div>
 
       <form onSubmit={onSubmit}>
-        <div className='mb-3'>
+        <div className='mb-2'>
           <Input label='Email' {...register('email')} />
           <p className='ml-1 mt-1 flex min-h-[20px] items-center gap-1 text-xs font-normal text-red-400'>
             {errors.email?.message}
           </p>
         </div>
 
-        <div className='mb-3'>
+        <div className='mb-2'>
           <Input label='Password' type='password' {...register('password')} />
           <p className='ml-1 mt-1 flex min-h-[20px] items-center gap-1 text-xs font-normal text-red-400'>
             {errors.password?.message}
--- src/utils/rules.ts
+++ src/utils/rules.ts
@@ -1,32 +1,46 @@
-import * as yup from 'yup'
+import * as zod from 'zod'
 
-const handleConfirmPasswordYup = (refString: string) => {
-  return yup
+const authValidation = {
+  email: zod
     .string()
-    .required('Nhập lại mật khẩu là bắt buộc')
+    .min(1, { message: 'Email là bắt buộc' })
+    .email('Email không đúng định dạng')
+    .max(160, 'Độ dài từ 5 - 160 ký tự'),
+  password: zod
+    .string()
+    .min(1, 'Mật khẩu là bắt buộc')
     .min(5, 'Độ dài từ 5 - 160 ký tự')
-    .max(160, 'Độ dài từ 5 - 160 ký tự')
-    .oneOf([yup.ref(refString)], 'Mật khẩu nhập lại không khớp')
+    .max(160, 'Độ dài từ 5 - 160 ký tự'),
+  confirm_password: zod
+    .string()
+    .min(1, 'Mật khẩu nhập lại là bắt buộc')
+    .min(5, 'Độ dài từ 5 - 160 ký tự')
+    .max(160, 'Độ dài từ 5 - 160 ký tự'),
+  first_name: zod.string().min(1, 'First name là bắt buộc').max(160, 'Độ dài tối đa là 160 kí tự'),
+  last_name: zod.string().min(1, 'Last name là bắt buộc').max(160, 'Độ dài tối đa là 160 kí tự'),
+  phone_number: zod.string().min(1, 'Số điện thoại là bắt buộc').max(20, 'Độ dài tối đa là 20 kí tự'),
+  address: zod.string().min(1, 'Địa chỉ là bắt buộc').max(160, 'Độ dài tối đa là 160 kí tự')
 }
 
-export const authSchema = yup.object({
-  email: yup
-    .string()
-    .required('Email là bắt buộc')
-    .matches(
-      // eslint-disable-next-line no-useless-escape
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Email không đúng định dạng'
-    )
-    // .email('Email không đúng định dạng')
-    .min(5, 'Độ dài từ 5 - 160 ký tự')
-    .max(160, 'Độ dài từ 5 - 160 ký tự'),
-  password: yup
-    .string()
-    .required('Mật khẩu là bắt buộc')
-    .min(5, 'Độ dài từ 5 - 160 ký tự')
-    .max(160, 'Độ dài từ 5 - 160 ký tự'),
-  confirm_password: handleConfirmPasswordYup('password')
+export const loginSchema = zod.object({
+  email: authValidation.email,
+  password: authValidation.password
 })
 
-export type AuthSchema = yup.InferType<typeof authSchema>
+export const registerSchema = zod
+  .object({
+    email: authValidation.email,
+    password: authValidation.password,
+    confirm_password: authValidation.confirm_password,
+    first_name: authValidation.first_name,
+    last_name: authValidation.last_name,
+    phone_number: authValidation.phone_number,
+    address: authValidation.address
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: 'Mật khẩu nhập lại không đúng',
+    path: ['confirm_password']
+  })
+
+export type LoginSchema = zod.infer<typeof loginSchema>
+export type RegisterSchema = zod.infer<typeof registerSchema>
