62315dfd3c15f96c812f841274b85508d388f72d
tainv2002 <taivannho5a@gmail.com>
2023-11-15 22:23:12 +0700

    Setup redux folders

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,14 +1,11 @@
+import { Outlet } from 'react-router-dom'
 import Header from 'src/components/Header'
 
-interface Props {
-  children?: React.ReactNode
-}
-
-function MainLayout({ children }: Props) {
+function MainLayout() {
   return (
     <div>
       <Header />
-      {children}
+      <Outlet />
     </div>
   )
 }
