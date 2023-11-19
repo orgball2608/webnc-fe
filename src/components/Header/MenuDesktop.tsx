d353c35ff330d6c9a8316d8d96fc98d5738382e6
Chu Nguyen <nguyenchuaqna@gmail.com>
2023-11-19 15:01:22 +0700

    fix: click menu

--- src/components/Header/MenuDesktop.tsx
+++ src/components/Header/MenuDesktop.tsx
@@ -8,10 +8,14 @@ export default function MenuDesktop({ anchorEl, handleMenuClose, handleSignout }
   return (
     <Menu id='user-menu' anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
       <MenuItem onClick={handleMenuClose}>
-        <Link to={path.profile}>Profile</Link>
+        <Link to={path.profile} className='w-full'>
+          Profile
+        </Link>
       </MenuItem>
       <MenuItem onClick={handleMenuClose}>
-        <Link to={path.change_password}>Change Password</Link>
+        <Link to={path.change_password} className='w-full'>
+          Change Password
+        </Link>
       </MenuItem>
       <MenuItem onClick={handleSignout}>Log Out</MenuItem>
     </Menu>
