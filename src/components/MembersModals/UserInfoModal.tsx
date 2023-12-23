867ea2550d03ebc2a2be4b6104f79846b9c7d858
Chu Nguyen <nguyenchuaqna@gmail.com>
2023-12-23 23:28:42 +0700

    improve UI info modal

--- src/components/MembersModals/UserInfoModal.tsx
+++ src/components/MembersModals/UserInfoModal.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Dialog, Card, CardBody } from '@material-tailwind/react'
 import { LuX } from 'react-icons/lu'
 import IconButton from 'src/components/IconButton'
@@ -5,8 +6,7 @@ import { Member } from 'src/apis/courses.api'
 
 interface Props {
   isInfoModalOpen: boolean
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  setIsInfoModalOpen: any
+  setIsInfoModalOpen: React.Dispatch<React.SetStateAction<boolean>>
   UserData: Member & {
     studentId?: string
   }
@@ -17,40 +17,43 @@ interface Props {
 
 function UserInfoModal({ isInfoModalOpen, setIsInfoModalOpen, UserData, isRole, role }: Props) {
   return (
-    <>
-      <Dialog
-        size='sm'
-        open={isInfoModalOpen}
-        handler={() => setIsInfoModalOpen(false)}
-        className='bg-transparent shadow-none'
-      >
-        <Card className='mx-auto w-full'>
-          <CardBody className='mb-8 ml-4 break-words text-lg'>
-            <div className='flex justify-end'>
-              <IconButton Icon={<LuX />} mode='dark' onClick={() => setIsInfoModalOpen(false)} />
-            </div>
-            <h4 className='my-5 text-3xl font-bold text-primary'>{`${UserData?.firstName} ${UserData?.lastName}`}</h4>
-            <div className='mb-4'>
+    <Dialog
+      size='sm'
+      open={isInfoModalOpen}
+      handler={() => setIsInfoModalOpen(false)}
+      className='bg-transparent shadow-none'
+    >
+      <Card className='mx-auto w-full'>
+        <CardBody className='mb-8 ml-4 break-words text-lg'>
+          <div className='flex justify-end'>
+            <IconButton Icon={<LuX />} mode='dark' onClick={() => setIsInfoModalOpen(false)} />
+          </div>
+          <h4 className='mb-7 text-2xl font-bold text-primary'>{`${UserData?.firstName} ${UserData?.lastName}`}</h4>
+          <table className='mb-4 w-full table-auto'>
+            <tbody>
               {isRole === role.students && (
-                <p className='mb-3'>
-                  <span className='font-bold'>Student ID: </span> {`${UserData?.studentId || 'N/A'}`}
-                </p>
+                <tr>
+                  <td className='border-b py-4 pr-4 font-bold'>Student ID:</td>
+                  <td className='border-b py-4'>{`${UserData?.studentId || 'N/A'}`}</td>
+                </tr>
               )}
-              <p className='mb-3'>
-                <span className='font-bold'>Email: </span> {`${UserData?.email || 'N/A'}`}
-              </p>
-              <p className='mb-3'>
-                <span className='font-bold'>Địa chỉ: </span>
-                {`${UserData?.address || 'N/A'}`}
-              </p>
-              <p className='mb-3'>
-                <span className='font-bold'>Số điện thoại: </span> {`${UserData?.phoneNumber || 'N/A'}`}
-              </p>
-            </div>
-          </CardBody>
-        </Card>
-      </Dialog>
-    </>
+              <tr>
+                <td className='border-b py-4 pr-4 font-bold'>Email:</td>
+                <td className='border-b py-4'>{`${UserData?.email || 'N/A'}`}</td>
+              </tr>
+              <tr>
+                <td className='border-b py-4 pr-4 font-bold'>Địa chỉ:</td>
+                <td className='border-b py-4'>{`${UserData?.address || 'N/A'}`}</td>
+              </tr>
+              <tr>
+                <td className='py-4 pr-4 font-bold'>Số điện thoại:</td>
+                <td className='py-4'>{`${UserData?.phoneNumber || 'N/A'}`}</td>
+              </tr>
+            </tbody>
+          </table>
+        </CardBody>
+      </Card>
+    </Dialog>
   )
 }
 
