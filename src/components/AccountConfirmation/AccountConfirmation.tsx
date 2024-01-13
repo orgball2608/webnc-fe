663fc962234f4649f55081463e9debbcea863ca4
tainv2002 <taivannho5a@gmail.com>
2024-01-13 14:33:19 +0700

    Fix chores

--- src/components/AccountConfirmation/AccountConfirmation.tsx
+++ src/components/AccountConfirmation/AccountConfirmation.tsx
@@ -1,4 +1,5 @@
 import { Button, Card, CardBody, CardFooter, Typography } from '@material-tailwind/react'
+import { useTranslation } from 'react-i18next'
 
 interface Props {
   email: string
@@ -7,18 +8,18 @@ interface Props {
 }
 
 function AccountConfirmation({ email, onSubmit, buttonTitle = 'Proceed' }: Props) {
+  const { t } = useTranslation()
   return (
     <div className='px-5'>
       <Card className='bg-primary text-white'>
         <CardBody>
           <Typography variant='h2' className='mb-8 text-center'>
-            Account Confirmation
+            {t('accountConfirmation')}
           </Typography>
           <Typography className='mb-3 text-center'>
-            An email with your account confirmation link has been sent to your email:{' '}
-            <b className='font-bold'>{email}</b>
+            {t('emailWithConfirmationLink')} <b className='font-bold'>{email}</b>
           </Typography>
-          <Typography className='text-center'>Check your email and comeback to proceed!</Typography>
+          <Typography className='text-center'>{t('checkEmailAndComeback')}</Typography>
         </CardBody>
         <CardFooter className='pt-0 text-center'>
           <Button size='lg' onClick={onSubmit}>
